759651843b667bf0ec529af86f84fb7b600f5081
767560479 <46210004+767560479@users.noreply.github.com>
2024-05-01 00:39:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -57,18 +57,19 @@ def definesys():
     headers = {
       'token': f'{token}'
     }
+    
    print('开始请求')
-  response = requests.request("GET", url, headers=headers)
-  print(response)
+   response = requests.request("GET", url, headers=headers)
+   print(response)
 
 # 检查响应状态码
-  if response.status_code == 200:
-      print('请求成功！')
-      # 处理响应数据
-      data = response.json()
-      print(data)
-  else:
-      print('请求失败，状态码：', response.status_code)
+   if response.status_code == 200:
+       print('请求成功！')
+       # 处理响应数据
+       data = response.json()
+       print(data)
+   else:
+       print('请求失败，状态码：', response.status_code)
     
 
 
